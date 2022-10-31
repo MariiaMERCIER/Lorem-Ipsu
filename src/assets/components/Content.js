51fcfcd77f25cc457463df2cc0965023acc3e7cb
Mariia IVANCHENKO <mariiaivanchenko@mbp-de-mariia.home>
2022-10-31 16:39:16 +0100

    avance

---
+++ src/assets/components/Content.js
@@ -0,0 +1,19 @@
+import Title from "./Title"
+import Description from "./Description"
+
+const Content= () => {
+    return (
+        <>
+      <div className="firstForm">
+     <Title/>
+     <Description/>
+      </div>
+      <div className="secondForm">
+     <Title/>
+     <Description/>
+      </div>
+      </>
+    );
+  }
+
+  export default Content;
---
+++ src/assets/components/Title.js
@@ -0,0 +1,10 @@
+const Title=() => {
+    return (
+      <div className="form">
+        <h1 className="firstTitle">Lorem</h1>
+        <h1 className="secondTitle">Ipsum</h1>
+      </div>
+    );
+  }
+
+  export default Title;
